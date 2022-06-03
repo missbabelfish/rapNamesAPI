1cec32f19073b4d71e1139d7f3f2321da06ca479
Leah Godfrey <missbabelfish@gmail.com>
2022-06-02 17:59:47 -0700

    working rapper api

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthname': "Sheyaa Joseph",
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthname': `Chancellor Bennett`,
+        'birthLocation': 'London, England'
+    },
+    'unknown': {
+        'age': 0,
+        'birthname': `unknown`,
+        'birth location': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    if( rappers[rapperName] ){
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers.unknown)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}. Wheeeee`)
+})
